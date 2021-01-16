a5d8b27d12c5d41e6eb1a05e33ea895e2093621e
Mateusz Olszewski <matcrono@gmail.com>
2021-01-16 18:39:18 +0100

    finished rendering Product

--- src/js/script.js
+++ src/js/script.js
@@ -52,16 +52,62 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product {
+    constructor (id, data) {
+
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
+      console.log('new Product', thisProduct)
+
+    }
+    renderInMenu() {
+      const thisProduct = this;
+
+      //generate HTML based on template //
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      //Create element using utils.createElementfromHTML //
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      // find menu container //
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      //add element to menu //
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+  }
+
+
   const app = {
-    init: function(){
+    initMenu: function(){
+      const thisApp = this;
+      console.log('ThisApp Data', thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product(productData,thisApp.data.products[productData])
+      }
+    },
+    initData: function() {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+
+    },
+    init: function() {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
   app.init();
+
 }
+

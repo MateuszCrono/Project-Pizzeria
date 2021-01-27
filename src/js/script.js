fe8ac2a238adf29d92c822ce19c8c94e475ed715
Mateusz Olszewski <matcrono@gmail.com>
2021-01-27 11:53:57 +0100

    Add new project files from website

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@
   const select = {
     templateOf: {
       menuProduct: "#template-menu-product",
+      cartProduct: '#template-cart-product',
     },
     containerOf: {
       menu: "#product-list",
@@ -28,10 +29,28 @@
     },
     widgets: {
       amount: {
-        input: 'input[name="amount"]',
+        input: 'input.amount',
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
+    },
+    cart: {
+      productList: '.cart__order-summary',
+      toggleTrigger: '.cart__summary',
+      totalNumber: `.cart__total-number`,
+      totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+      deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+      form: '.cart__order',
+      formSubmit: '.cart__order [type="submit"]',
+      phone: '[name="phone"]',
+      address: '[name="address"]',
+    },
+    cartProduct: {
+      amountWidget: '.widget-amount',
+      price: '.cart__product-price',
+      edit: '[href="#edit"]',
+      remove: '[href="#remove"]',
     },
   };
 
@@ -39,6 +58,9 @@
     menuProduct: {
       wrapperActive: "active",
       imageVisible: "active",
+    },
+    cart: {
+      wrapperActive: 'active',
     },
   };
 
@@ -48,12 +70,14 @@
       defaultMin: 1,
       defaultMax: 10,
     },
+    cart: {
+      defaultDeliveryFee: 20,
+    },
   };
 
   const templates = {
-    menuProduct: Handlebars.compile(
-      document.querySelector(select.templateOf.menuProduct).innerHTML
-    ),
+      menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+      cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   };
 
   class Product {

b1b764a49194e8ca00bb80748dfcfe7051930256
Mateusz Olszewski <matcrono@gmail.com>
2021-02-19 16:00:24 +0100

    minor isssue fix

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -1,6 +1,6 @@
 
 import {select, classNames, templates} from '../settings.js';
-import utils from '../utils.js';
+import {utils} from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 
 class Product {

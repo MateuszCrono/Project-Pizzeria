cab15d4926fb91ab6f5b2b8a5a470fc77e79810f
Mateusz Olszewski <matcrono@gmail.com>
2021-03-08 14:43:38 +0100

    prepare heroku app

--- src/js/settings.js
+++ src/js/settings.js
@@ -119,7 +119,7 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     product: 'product',
     order: 'order',
     booking: 'booking',
